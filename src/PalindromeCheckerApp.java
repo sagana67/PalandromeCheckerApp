8d924dedfaf21311550f4aa96fcbafd29770a160
Sagana67 <saganadhiyana@gmail.com>
2026-02-25 15:08:04 +0530

    UC3:palindrome reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,15 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String word = "madam";
-        String reverse = new StringBuilder(word).reverse().toString();
+        String input = "madam";
+        String reversed = "";
 
-        if(word.equals(reverse))
-            System.out.println(word + " is a palindrome");
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed))
+            System.out.println("Palindrome");
         else
-            System.out.println(word + " is not a palindrome");
+            System.out.println("Not Palindrome");
     }
 }
